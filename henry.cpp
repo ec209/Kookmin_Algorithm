55949002ed4fbc7f0f5e6b3168d901aa08fb1673
TAEGI <felimox.kim@gmail.com>
2019-10-29 23:12:55 -0400

    recursive

---
+++ henry.cpp
@@ -0,0 +1,36 @@
+//
+//  henry.cpp
+//  
+//
+//  Created by TAEGIKIM on 21/09/2017.
+//
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int original, temp;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        cin >> a >> b;
+        while(1)
+        {
+            orignlal = a/b;
+            temp = a/(b++);
+            original = original - temp; // using recursive algorithm
+            if(original == 0)
+                break;
+        }
+        
+        
+    }
+}
