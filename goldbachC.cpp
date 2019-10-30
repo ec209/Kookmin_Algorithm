15d5679ee10ac3def98a07cf01eda1cfc476a1b0
TAEGI <felimox.kim@gmail.com>
2019-10-29 23:11:54 -0400

    binary search

---
+++ goldbachC.cpp
@@ -0,0 +1,53 @@
+//
+//  goldbachC.cpp
+//  
+//
+//  Created by TAEGIKIM on 29/09/2017.
+//
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+void setting();
+
+vector<int>pNums;
+bool pNum[10001] = { false, };
+
+
+int main(){
+    setting();
+    int t; cin >> t;
+    while (t--){
+        int  a, b,n; cin >> n;
+        
+        for (int i = 0; i < pNums.size() && pNums[i] <= n/2; i++){
+            if (binary_search(pNums.begin(), pNums.end(), n - pNums[i])){
+                a = pNums[i]; // b = n - pNum[i];
+            }
+            
+        }
+        cout << a << ' ' << n-a << endl;
+    }
+    return 0;
+}
+
+
+void setting(){
+    int sqrtn = sqrt(10001);
+    for (int i = 2; i <= sqrtn; i++)
+        if (!pNum[i]){
+            pNums.push_back(i);
+            for (int j = i*i; j < 10001; j += i)
+                pNum[j] = true;
+        }
+    for (int i = sqrtn + 1; i < 10001; i++)
+        if (!pNum[i])
+            pNums.push_back(i);
+}
