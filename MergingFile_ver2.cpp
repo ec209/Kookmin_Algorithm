f6c3db4be1ea779d8afaf8338ee94bc1e28efe26
TAEGI <felimox.kim@gmail.com>
2019-11-06 20:32:32 -0500

    recusive_dynamicProgramming

---
+++ MergingFile_ver2.cpp
@@ -0,0 +1,42 @@
+//
+//  MergingFile_ver2.cpp
+//  
+//
+//  Created by TAEGIKIM on 11/11/2017.
+//
+//
+
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int T, N;
+int A[503], S[503];
+int D[503][503], K[503][503];
+
+int main()
+{
+    for (scanf("%d", &T);T--;){
+        scanf("%d", &N);
+        for (int i=1;i<=N;i++) scanf("%d", A+i), S[i] = S[i-1] + A[i];
+        for (int i=1;i<=N;i++) D[i-1][i] = 0, K[i-1][i] = i;
+        for (int d=2;d<=N;d++){
+            // d = j-i
+            for (int i=0;i+d<=N;i++){
+                int j = i+d;
+                D[i][j] = 2e9;
+                for (int k=K[i][j-1];k<=K[i+1][j];k++){
+                    int v = D[i][k] + D[k][j] + S[j] - S[i];
+                    if (D[i][j] > v)
+                    D[i][j] = v, K[i][j] = k;
+                }
+            }
+        }
+        printf("%d\n", D[0][N]);
+    }
+}
+
+
+
+
